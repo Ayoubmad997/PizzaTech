cc7add173bbfa6abfcf03886ace5d69fd3f6a31b
Younesx207 <younes4638@gmail.com>
2025-06-04 14:18:34 +0200

    frontend first commit

--- cicd/back/server.js
+++ cicd/back/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import cors from cors;
  
 const app = express();
+app.arguments(cors());
  
 app.get('/api', (req, res) => {
   res.json({text : 'Hello World from api !'});
---
+++ cicd/front/app.js
@@ -0,0 +1,11 @@
+
+const btn = document.querySelector(".btn");
+const title = document.querySelector("h1");
+
+    btn.addEventListener("click", () => {
+        fetch("http://localhost:3000/api")
+        .then((res) => res.json())
+        .then((data) => {
+            title.innerHTML = data.title;
+        });
+    });
